fbabc34bcd0c1789e73921a88ebd05af0b681352
Partha <softaweb13@gmail.com>
2021-08-16 14:35:35 +0530

    final Project

--- ins-order.php
+++ ins-order.php
@@ -14,7 +14,7 @@ $sa=$_POST['sa'];
 $odate=time();
 $cid=$_SESSION['cid'];
 
-$ins="INSERT INTO morder SET bn='$bn',be='$be',bp='$bp',ba='$ba',sn='$sn',se='$se',sp='$sp',sa='$sa',odate='$odate',cid='$cid'";
+$ins="INSERT INTO morder SET bn='$bn',be='$be',bp='$bp',ba='$ba',sn='$sn',se='$se',sp='$sp',sa='$sa',odate='$odate',cid='$cid',payemt_status='success'";
 $con->query($ins);
  $moid = $con->insert_id;
 
@@ -38,61 +38,7 @@ $sel="SELECT * FROM cart WHERE cid='$cid'";
       	$del="DELETE FROM cart WHERE cid='$cid'";
       	$con->query($del);
 
+      	header("location:thanks.php");
+
 ?>
 
-
-<form method="post" action="pgRedirect.php" id="frm" style="display: none;">
-		<table border="1">
-			<tbody>
-				<tr>
-					<th>S.No</th>
-					<th>Label</th>
-					<th>Value</th>
-				</tr>
-				<tr>
-					<td>1</td>
-					<td><label>ORDER_ID::*</label></td>
-					<td><input id="ORDER_ID" tabindex="1" maxlength="20" size="20"
-						name="ORDER_ID" autocomplete="off"
-						value="<?php echo  "ORDS" . rand(10000,99999999)?>">
-					</td>
-				</tr>
-				<tr>
-					<td>2</td>
-					<td><label>CUSTID ::*</label></td>
-					<td><input id="CUST_ID" tabindex="2" maxlength="12" size="12" name="CUST_ID" autocomplete="off" value="CUST001"></td>
-				</tr>
-				<tr>
-					<td>3</td>
-					<td><label>INDUSTRY_TYPE_ID ::*</label></td>
-					<td><input id="INDUSTRY_TYPE_ID" tabindex="4" maxlength="12" size="12" name="INDUSTRY_TYPE_ID" autocomplete="off" value="Retail"></td>
-				</tr>
-				<tr>
-					<td>4</td>
-					<td><label>Channel ::*</label></td>
-					<td><input id="CHANNEL_ID" tabindex="4" maxlength="12"
-						size="12" name="CHANNEL_ID" autocomplete="off" value="WEB">
-					</td>
-				</tr>
-				<tr>
-					<td>5</td>
-					<td><label>txnAmount*</label></td>
-					<td><input title="TXN_AMOUNT" tabindex="10"
-						type="text" name="TXN_AMOUNT"
-						value="<?php echo $total; ?>">
-					</td>
-				</tr>
-				<tr>
-					<td></td>
-					<td></td>
-					<td><input value="CheckOut" type="submit"	onclick=""></td>
-				</tr>
-			</tbody>
-		</table>
-		* - Mandatory Fields
-	</form>
-
-
-	<script>
-		document.getElementById("frm").submit();
-	</script>
